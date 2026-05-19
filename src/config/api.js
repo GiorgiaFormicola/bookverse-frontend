f3bbac134c3674b9b51cca583578b37744eeafa9
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-05-19 18:23:12 +0200

    Adds redux dependencies

--- src/config/api.js
+++ src/config/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    Authorization: `Bearer ${localStorage.getItem}`,
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
 });
